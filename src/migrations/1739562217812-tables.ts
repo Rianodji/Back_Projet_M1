7a4b23ca6153b5bcac38afefa04b5816067dcad9
Rianodji Dicard <dicardrianodji@gmail.com>
2025-02-14 20:58:00 +0100

    fix render migrations

--- src/migrations/1739562217812-tables.ts
+++ src/migrations/1739562217812-tables.ts
@@ -4,6 +4,12 @@ export class Tables1739562217812 implements MigrationInterface {
     name = 'Tables1739562217812'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        // 🔥 Supprimer les tables si elles existent
+        await queryRunner.query(`DROP TABLE IF EXISTS "user_roles_role" CASCADE`);
+        await queryRunner.query(`DROP TABLE IF EXISTS "user" CASCADE`);
+        await queryRunner.query(`DROP TABLE IF EXISTS "role" CASCADE`);
+    
+        // 🎉 Recréer les tables proprement
         await queryRunner.query(`CREATE TABLE "role" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "UQ_ae4578dcaed5adff96595e61660" UNIQUE ("name"), CONSTRAINT "PK_b36bcfe02fc8de3c57a8b2391c2" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "first_name" character varying NOT NULL, "last_name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "last_password" character varying, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP DEFAULT now(), "last_login" TIMESTAMP, CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "user_roles_role" ("userId" integer NOT NULL, "roleId" integer NOT NULL, CONSTRAINT "PK_b47cd6c84ee205ac5a713718292" PRIMARY KEY ("userId", "roleId"))`);
