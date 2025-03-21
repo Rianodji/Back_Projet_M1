4391504ae953b0e56072bb84ec4ab76e7a165ed3
Rianodji Dicard <dicardrianodji@gmail.com>
2025-03-21 18:52:51 +0100

    resolution de conflits

--- src/inscription/inscription.module.ts
+++ src/inscription/inscription.module.ts
@@ -1,19 +1,23 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Inscription } from './entities/inscription.entity'; // Import de l'entité Inscription
+import { Inscription } from './entities/inscription.entity'; 
 import { InscriptionService } from './inscription.service';
 import { InscriptionController } from './inscription.controller';
 import { EquipesModule } from '../equipes/equipes.module';
 import { JoueurModule } from '../joueur/joueur.module';
+import { AuthModule } from '../auth/auth.module';
+import { CaslModule } from 'casl/casl.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Inscription]),
     forwardRef(()=>EquipesModule),
-    forwardRef(()=>JoueurModule)
+    forwardRef(()=>JoueurModule),
+    forwardRef(()=>AuthModule),
+    forwardRef(()=> CaslModule)
   ],
   controllers: [InscriptionController],
   providers: [InscriptionService],
-  exports:[InscriptionService]
+  exports:[InscriptionService, TypeOrmModule.forFeature([Inscription])]
 })
 export class InscriptionModule {}
